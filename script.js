21aa157ea1422360dcdd68c3a592de5c81390d3b
Domenic Beall <domenicbeall2@gmail.com>
2020-12-14 21:37:23 +1100

    Added results length functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#searchbtn").on("click", function() {
 
     var searchTerm = $("#search").val().trim();
-    var numberRes = $("#numberRes").val().trim();
+    var numberRes = Number($("#numberRes").val().trim());
     var startYear = $("#startY").val().trim();
     var endYear = $("#endY").val().trim();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=g0t77IZKEPbYqicc66eUR3TOBTkQGAje";
@@ -21,7 +21,7 @@ $("#searchbtn").on("click", function() {
      .then(function(response) {
         var docs = response.response.docs;
 
-        for (let i = 0; i < docs.length; i++) {
+        for (let i = 0; i < numberRes; i++) {
             var articleDiv = $("<div class=\"card\">");
             articleDiv.text(docs[i].abstract);
             $("#articles").append(articleDiv);
