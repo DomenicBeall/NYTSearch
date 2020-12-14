25e5d120d2563cea31d0fb6f2d55c5ba03c95dc1
Domenic Beall <domenicbeall2@gmail.com>
2020-12-14 21:35:31 +1100

    Updated index.html, script.js and style.css

---
+++ script.js
@@ -0,0 +1,30 @@
+$("#searchbtn").on("click", function() {
+
+    var searchTerm = $("#search").val().trim();
+    var numberRes = $("#numberRes").val().trim();
+    var startYear = $("#startY").val().trim();
+    var endYear = $("#endY").val().trim();
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=g0t77IZKEPbYqicc66eUR3TOBTkQGAje";
+
+    if (startYear !== "") {
+        queryURL += "&begin-date=" +  startYear;
+    }
+
+    if (endYear !== "") {
+        queryURL += "&end-date=" + endYear;
+    }
+
+    $.ajax({
+       url: queryURL,
+       method: "GET"
+     })
+     .then(function(response) {
+        var docs = response.response.docs;
+
+        for (let i = 0; i < docs.length; i++) {
+            var articleDiv = $("<div class=\"card\">");
+            articleDiv.text(docs[i].abstract);
+            $("#articles").append(articleDiv);
+        }
+     });
+});
